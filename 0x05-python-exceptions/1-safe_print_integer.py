09f7fbe8e5255bc819058c438c551c4f02834949
raheldaniel14 <99663820+raheldaniel14@users.noreply.github.com>
2022-06-22 11:18:31 +0100

    Update 1-safe_print_integer.py

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,14 +1,7 @@
 #!/usr/bin/python3
-def magic_calculation(a, b):
-    result = 0
-    for i in range(1, 3):
-        try:
-            if i > a:
-                raise Exception('Too far')
-            else:
-                result += (a ** b) / i
-        except:
-            result = b + a
-            break
-    return result
-
+def safe_print_integer(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except:
+        return False
